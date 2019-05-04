6698532c3ce804919b60236f35c843ef8de4f694
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-05-04 17:55:00 +0200

    Mockito: add valid user test.

--- src/test/java/MockitoTest.java
+++ src/test/java/MockitoTest.java
@@ -1,10 +1,16 @@
+import models.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import repositories.IBookRepository;
 import repositories.IReservedBookRepository;
 import repositories.IUserRepository;
 import services.LibraryService;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
 public class MockitoTest {
 
@@ -21,6 +27,18 @@ public class MockitoTest {
         service = new LibraryService(userRepository, bookRepository, reservedBookRepository);
     }
 
+    @Test
+    public void addValidUserReturnsTrue() {
+        User user = new User();
+
+        doReturn(false).when(userRepository).userExists(user.getLogin());
+        doReturn(true).when(userRepository).validateUser(any(User.class));
+
+        boolean result = service.addUser(user.getLogin(), user.getPassword());
+
+        assertThat(result).isTrue();
+    }
+    
     @AfterEach
     public void tearDown() {
         userRepository = null;
