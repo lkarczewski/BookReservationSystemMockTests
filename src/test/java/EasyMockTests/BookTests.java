99fc5d6081bba8e0e10876dd39be1cc70ea21a26
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-05-05 12:58:44 +0200

    EasyMock: add book tests.

--- src/test/java/EasyMockTests/BookTests.java
+++ src/test/java/EasyMockTests/BookTests.java
@@ -4,10 +4,17 @@ import models.Book;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import repositories.IBookRepository;
 import repositories.IReservedBookRepository;
 import repositories.IUserRepository;
 import services.LibraryService;
+
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BookTests {
 
@@ -25,6 +32,28 @@ public class BookTests {
         service = new LibraryService(userRepository, bookRepository, reservedBookRepository);
         book = new Book();
     }
+
+    @Test
+    void addNonExistingValidBookReturnsTrue() {
+        expect(bookRepository.validateBook(anyObject(Book.class))).andReturn(true);
+        expect(bookRepository.bookExists("title")).andReturn(false);
+        replay(bookRepository);
+
+        boolean result = service.addBook(book.getTitle(), book.getAuthor(), book.getGenre(), book.getDescription());
+        assertTrue(result);
+    }
+
+    @Test
+    void addInvalidBookThrowsIllegalArgumentException() {
+        expect(bookRepository.validateBook(anyObject(Book.class))).andReturn(false);
+        replay(bookRepository);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.addBook(book.getTitle(), book.getTitle(), book.getGenre(), book.getDescription());
+        });
+    }
+
+    
 
     @AfterEach
     void tearDown() {
